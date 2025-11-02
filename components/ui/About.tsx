96b5d4e8b6c45c461cb744e39bf49304edd9b5c1
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2025-11-02 12:11:40 +0700

    style: apply custom styling to About page text

--- components/ui/About.tsx
+++ components/ui/About.tsx
@@ -1,12 +1,11 @@
+import MyStyle from '@/Style';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function About() {
   return (
     <View>
-      <Text>About Page</Text>
+      <Text style={MyStyle.text}>About Page</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
